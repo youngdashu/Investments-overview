254183351066c74eab7995a02087ca5d11c4bcfd
youngdashu <anowak2000@gmail.com>
2021-08-30 22:52:41 +0200

    update klasy inwestycja

--- src/investmentData/k1.py
+++ src/investmentData/k1.py
@@ -1,66 +1,82 @@
-class_count=0
-class main_char:
-    price: int=0
-    area=0
-    try:
-        ppm=price/area
-    except:
-        ppm=0
-class info:
-    date_start=""
-    date_end=""
-    desc=""
-    address1=""
-    address2=""
-    estimated_value=0
-    estimation_date=""
-class contribution:
-    contr=0
-    marg_b=0
-    marg_n=0
-    tax=0
-    other=0
-    redec=0
-    total=contr+marg_b+marg_n+tax+other+redec
-    purchase=0
-class credit:
-    bank_c=0
-    bank_perc=0
-    rate=0
-    time=0
-    month_rate=0
-    capital_part=0
-    interest_part=0
-    monthly_insurance=0
-    costs_total=0
-class rent:
-    income_min=0
-    income_max=0
-    income_real=0
-    tax_rent=0
-    tax_estate=0
-    power=0
-    gas=0
-    water=0
-    internet=0
-    other=0
-    cost_total=tax_estate+tax_rent+power+gas+water+other
-    gain=income_real-cost_total
-class eval:
-    return_time=0
-    return_total=0
-    return_rate=0
-    rate_total=0
-    notes=""
-class stats:
-    num=class_count+1
-    stat1=main_char()
-    stat2=info()
-    stat3=contribution()
-    try:
-        stat3.purchase=stat3.total/stat1.price
-    except:
-        pass
-    stat4=credit()
-    stat5=rent()
-    stat6=eval()
+import json
+class Main_Char:
+    def __init__(self,price,area,ppm):
+        self.price=price
+        self.area=area
+        self.ppm=ppm
+class Info:
+    def __init__(self,date_start,date_end,desc,address1,address2,estimated_value,estimation_date):
+        self.date_start=date_start
+        self.date_end=date_end
+        self.desc=desc
+        self.address1=address1
+        self.address2=address2
+        self.estimated_value=estimated_value
+        self.estimation_date=estimation_date
+class Contribution:
+    def __init__(self,contr,marg_b,marg_n,tax,other,redec,total,purchase):
+        self.contr=contr
+        self.marg_b=marg_b
+        self.marg_n=marg_n
+        self.tax=tax
+        self.other=other
+        self.redec=redec
+        self.total=total
+        self.purchase=purchase
+class Credit:
+    def __init__(self,bank_c,bank_perc,rate,time,month_rate,capital_part,interest_part,monthly_insurance,costs_total):
+        self.bank_c=bank_c
+        self.bank_perc=bank_perc
+        self.rate=rate
+        self.time=time
+        self.month_rate=month_rate
+        self.capital_part=capital_part
+        self.interest_part=interest_part
+        self.monthly_insurance=monthly_insurance
+        self.costs_total=costs_total
+class Rent:
+    def __init__(self,income_min,income_max,income_real,tax_rent,tax_estate,power,gas,water,internet,other,cost_total,gain):
+        self.income_min=income_min
+        self.income_max=income_max
+        self.income_real=income_real
+        self.tax_rent=tax_rent
+        self.tax_estate=tax_estate
+        self.power=power
+        self.gas=gas
+        self.water=water
+        self.internet=internet
+        self.other=other
+        self.cost_total=cost_total
+        self.gain=gain
+class Eval:
+    def __init__(self,return_time,return_total,return_rate,rate_total,notes):
+        self.return_time=return_time
+        self.return_total=return_total
+        self.return_rate=return_rate
+        self.rate_total=rate_total
+        self.notes=notes
+class Investment:
+    def __init__(self,title):
+        self.title=title
+        self.Main_Char=Main_Char(0,0,0)
+        self.Info=Info(0,0,"","","",0,0)
+        self.Contribution=Contribution(0,0,0,0,0,0,0,0)
+        self.Credit=Credit(0,0,0,0,0,0,0,0,0)
+        self.Rent=Rent(0,0,0,0,0,0,0,0,0,0,0,0)
+        self.Eval=Eval(0,0,0,0,"")
+    def save(self):
+        self.inv=json.dumps(self.Main_Char.__dict__)+json.dumps(self.Info.__dict__)+json.dumps(self.Contribution.__dict__)+json.dumps(self.Credit.__dict__)+json.dumps(self.Rent.__dict__)+json.dumps(self.Eval.__dict__)
+        guide=open("guide.txt","r")
+        line=guide.readlines()
+        num=int(''.join(line[-1:]))
+        guide.close()
+        guide = open("guide.txt", "a")
+        guide.write('\n'+str(num+1)+": "+str(self.title))
+        guide.close()
+        outfile=open("data.txt", "a")
+        outfile.write("IStart__"+str(num)+"__")
+        outfile.write(json.dumps(self.inv))
+        outfile.write("__End__")
+        outfile.close()
+g=Investment("Test")
+g.save()
